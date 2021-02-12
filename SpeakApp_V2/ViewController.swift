144af83dd297d32ccb1a1d4e7bb0c9ebb6d67fcf
FredFu <fjw08199999@gmail.com>
2021-02-13 06:26:52 +0800

    新增畫面完成 以及 設定按鈕連結至Main.StoryBoard

--- SpeakApp_V2/ViewController.swift
+++ SpeakApp_V2/ViewController.swift
@@ -10,12 +10,43 @@ import AVFoundation
 
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var setLangunge: UISegmentedControl!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var langungeSpeedLabel: UILabel!
+    @IBOutlet weak var langungePitchMultiplierLabel: UILabel!
+    @IBOutlet weak var langungeVolumeLabel: UILabel!
+    
+    @IBOutlet weak var speedLabelValue: UILabel!
+    @IBOutlet weak var pitchMultiplierLabelValue: UILabel!
+    @IBOutlet weak var volumeLabelValue: UILabel!
+    
+    @IBOutlet weak var speedSliderValue: UISlider!
+    @IBOutlet weak var pitchMultiplierSliderValue: UISlider!
+    @IBOutlet weak var volumeSliderValue: UISlider!
+    
+    @IBOutlet weak var randomButtenSet: UIButton!
+    @IBOutlet weak var speakButtenSet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    @IBAction func speedSlider(_ sender: Any) {
+    }
+    
+    @IBAction func pitchMultiplierSlider(_ sender: Any) {
+    }
+    
+    @IBAction func volumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func randomButten(_ sender: Any) {
+    }
+    
+    @IBAction func speakButten(_ sender: Any) {
+    }
 }
 
